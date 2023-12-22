d68465513d94f7a2acbc097ba4f0768928e6107e
Benwaz78 <nonwaz78@gmail.com>
2023-12-22 10:52:14 +0100

    New changes

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+
+
+const getData = async ()=>{
+    const response = await fetch("http://student-api.softwareacademy.ng/api/");
+    try {
+        const row = document.getElementById("row");
+        row.innerHTML = ``;
+        if(!response.ok){
+            throw new Error("Could not fetch data");
+        }
+        const data = await response.json();
+        console.log(data);
+        data.map(item=>{
+            row.innerHTML += `
+            <div class="column">
+                <div class="box-img" style="background:url(${item.menu_img}) no-repeat center/cover">
+                
+                </div>
+                
+                <h2>${item.menu_name}</h2>
+                <p>
+                    ${item.description}
+                </p>
+                <a href="#">readmore</a>
+            </div>
+            
+            `
+        })
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+getData();
